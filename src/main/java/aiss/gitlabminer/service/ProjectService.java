668f68911fb78d0649dc00fe99fd46d06624c378
josmorlop10 <josmorlop10@alum.us.es>
2023-04-28 19:20:42 +0200

    ProjectService con token

--- src/main/java/aiss/gitlabminer/service/ProjectService.java
+++ src/main/java/aiss/gitlabminer/service/ProjectService.java
@@ -2,6 +2,10 @@ package aiss.gitlabminer.service;
 
 import aiss.gitlabminer.model.Project;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,10 +18,21 @@ public class ProjectService {
     @Autowired
     RestTemplate restTemplate;
 
-    public List<Project> findAllProjects(){
-        String uri = "https://gitlab.com/api/v4/projects";
-        Project[] projects = restTemplate.getForObject(uri, Project[].class);
-        return Arrays.stream(projects).toList();
-    }
+    //    @Value("${githubminer.baseuri}")
+    private String getAll=  "https://gitlab.com/api/v4/projects";
 
+    //@Value("${githubminer.token}")
+    private String token = "glpat-ZA3FgXiyUYW4tegWNbC1";
+
+    public ResponseEntity<Project[]> findAllProjects(){
+
+        HttpHeaders headers = new HttpHeaders();
+        if(token != "") {
+            headers.set("Authorization", "Bearer " + token);
+        }
+        HttpEntity<Project[]> request = new HttpEntity<>(null, headers);
+        ResponseEntity<Project[]> response = restTemplate
+                .exchange(getAll, HttpMethod.GET, request, Project[].class);
+        return response;
+    }
 }
