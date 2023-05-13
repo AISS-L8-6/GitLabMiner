86634ed00a7a161c45e72fd68c039f09df7f9932
javruifer1 <89843770+javruifer1@users.noreply.github.com>
2023-05-13 21:31:41 +0200

    commit del makina

--- src/main/java/aiss/gitlabminer/controller/projectController.java
+++ src/main/java/aiss/gitlabminer/controller/projectController.java
@@ -1,14 +1,19 @@
 package aiss.gitlabminer.controller;
 
 import aiss.gitlabminer.model.Commit;
+import aiss.gitlabminer.model.Project;
 import aiss.gitlabminer.model.comment.Comment;
 import aiss.gitlabminer.model.issue.Issue;
-import aiss.gitlabminer.model.Project;
 import aiss.gitlabminer.model.parse.CommentParse;
 import aiss.gitlabminer.model.parse.IssueParse;
 import aiss.gitlabminer.model.parse.ProjectParse;
 import aiss.gitlabminer.model.parse.UserParse;
 import aiss.gitlabminer.service.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -37,7 +42,14 @@ public class projectController {
 
     @Autowired
     UserService userService;
-
+    @Operation(
+            summary = "Get project by id",
+            description = "Get project by id",
+            tags = {"project", "get"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Success ", content = {@Content(schema = @Schema(implementation = projectController.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = {@Content(schema = @Schema())})
+    })
     @GetMapping("/apipath/project/{id}")
     public ProjectParse getById(@PathVariable String id, @RequestParam(name = "sinceCommits", required = false) Integer sinceCommits, @RequestParam(name = "sinceIssues", required = false) Integer sinceIssues, @RequestParam(name = "maxPages", required = false) Integer maxPages) {
         ProjectParse result;
@@ -77,6 +89,15 @@ public class projectController {
     }
 
 
+
+    @Operation(
+            summary = "Insert a project",
+            description = "Add a project whose data is built from the GitLab API",
+            tags = {"project", "post"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Created", content = {@Content(schema = @Schema(implementation = projectController.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = {@Content(schema = @Schema())})
+    })
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/apipath/project/{id}")
     public ProjectParse postById(@PathVariable String id, @RequestParam(name = "sinceCommits", required = false) Integer sinceCommits, @RequestParam(name = "sinceIssues", required = false) Integer sinceIssues, @RequestParam(name = "maxPages", required = false) Integer maxPages) {
